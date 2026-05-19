41ec830a9f6f3ed4a5bff6d4e064397081713e99
Ana Anselmi <anaanselmi0@gmail.com>
2026-05-19 12:20:30 -0400

    fix: move js folder inside TAREA project structure

---
+++ js/app.js
@@ -0,0 +1,6 @@
+import { inicializarTienda } from './ui.js';
+
+// Arrancar la aplicación cuando cargue la página
+document.addEventListener('DOMContentLoaded', () => {
+    inicializarTienda();
+}); 
